933d1a812a32285caaa642187522d5e4d9a589f8
alexyndr <alexyndr@gmail.com>
2019-07-15 13:36:02 +0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem  'codebreaker', github: 'alexyndr/codebreaker'
+
+gem  'rake', '~> 10.0'
+gem  'rspec', '~> 3.7'
+gem  'fasterer'
+gem  'pry'
+gem  'rubocop'
+gem  'rubocop-rspec'
+gem  'simplecov'
---
+++ lib/racker.rb
@@ -0,0 +1,18 @@
+require 'erb'
+
+class Racker
+  def call(env)
+    request = Rack::Request.new(env)
+    case request.path
+    when '/' then Rack::Response.new(render('index.html.erb'))
+    else Rack::Response.new('Not Found', 404)
+    end
+  end
+
+  private
+
+  def render(template)
+    path = File.expand_path("../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
+  end
+end
